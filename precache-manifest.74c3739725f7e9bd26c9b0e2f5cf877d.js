e60c480ec139cbe1e13c27e7572978e8b8d87047
Ravi Kumar Prasad <7ravikp@gmail.com>
2020-03-27 00:12:01 +0530

    Updates

---
+++ precache-manifest.74c3739725f7e9bd26c9b0e2f5cf877d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a90db16a20cc1336ce268aebc89d4e8c",
+    "url": "/simple-app/index.html"
+  },
+  {
+    "revision": "1c889caf6738ffb66608",
+    "url": "/simple-app/static/css/main.e06c322f.chunk.css"
+  },
+  {
+    "revision": "b3d7ceaa5c9f113789db",
+    "url": "/simple-app/static/js/2.ea6f282a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-app/static/js/2.ea6f282a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c889caf6738ffb66608",
+    "url": "/simple-app/static/js/main.7fa6e719.chunk.js"
+  },
+  {
+    "revision": "fbc221b9db3024774393",
+    "url": "/simple-app/static/js/runtime-main.72f994ce.js"
+  },
+  {
+    "revision": "e405b94d654cd3575b139e452ccfc9c9",
+    "url": "/simple-app/static/media/background.e405b94d.jpg"
+  },
+  {
+    "revision": "92ecfa0adf92f160c4e8d6fab44581a5",
+    "url": "/simple-app/static/media/logo-name.92ecfa0a.png"
+  }
+]);
